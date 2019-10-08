45588f85feef5a9e382a90fee1d2402c58ecb38f
fatindelfiand <50041219+fatindelfiand@users.noreply.github.com>
2019-10-08 21:06:36 +0700

    menambahkan latihan minggu ke-6

---
+++ 2.php
@@ -0,0 +1,39 @@
+<?php
+$a=15;
+$b=5;
+
+echo "bilangan 1 = $a";
+echo "<br>";
+echo "bilangan 2 = $b";
+echo "<br>";
+
+echo "<br/> PENJUMLAHAN <br/>";
+$c=$a+$b;
+echo "operator = +"; echo "<br>";
+echo "hasilnya : $a+$b=$c";
+echo "<br/>";
+
+echo "<br/> PENGURANGAN <br/>";
+$c=$a-$b;
+echo "operator = -"; echo "<br>";
+echo "hasilnya : $a-$b=$c";
+echo "<br/>";
+
+echo "<br/> PERKALIAN <br/>";
+$c=$a*$b;
+echo "operator = *"; echo "<br>";
+echo "hasilnya : $a*$b=$c";
+echo "<br/>";
+
+echo "<br/> PEMBAGIAN <br/>";
+$c=$a/$b;
+echo "operator = /"; echo "<br>";
+echo "hasilnya : $a/$b=$c";
+echo "<br/>";
+
+echo "<br/> MODULUS <br/>";
+$c=$a%$b;
+echo "operator = %"; echo "<br>";
+echo "hasilnya : $a%$b=$c";
+echo "<br/>";
+?>
